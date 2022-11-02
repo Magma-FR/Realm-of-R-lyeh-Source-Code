63fb051903c740f13c19b42609ea96b66226a90c
Magma-FR <117277406+Magma-FR@users.noreply.github.com>
2022-11-03 01:11:29 +1000

    OutcropSpawn is weird asf
    Outcrop spawn will be changed when i feel like it lol

---
+++ Tiles/OutcropSpawn1.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using RealmOne.Tiles;
+using Terraria.DataStructures;
+
+namespace RealmOne.Tiles
+{
+    internal class OutcropSpawn1 : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            TileID.Sets.AllTiles[Type] = true;
+
+            Main.tileSolid[Type] = true;
+            
+
+
+            AddMapEntry(new Color(170, 146, 185), CreateMapEntryName());
+
+            DustType = DustID.Tungsten;
+            ItemDrop = ModContent.ItemType<Items.Placeables.Outcrop>();
+
+            HitSound = SoundID.Tink;
+            
+            MineResist = 1.5f;
+            MinPick = 100;
+        }
+    }
+}
